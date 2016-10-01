a2568d39bfdfff6330ef04d5673c88ce7e2582fa
Vikas Kumar <vrnvikas1994@gmail.com>
2016-10-02 02:31:26 +0530

    added more screens

--- app/src/main/java/com/campaignar/smis/campaignar/Activity/KnowYourCandidate.java
+++ app/src/main/java/com/campaignar/smis/campaignar/Activity/KnowYourCandidate.java
@@ -1,15 +1,54 @@
 package com.campaignar.smis.campaignar.Activity;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.campaignar.smis.campaignar.Fragments.FragmentKnowYourCandidate;
+import com.campaignar.smis.campaignar.Fragments.FragmentKnowYourCandidateDetail;
+import com.campaignar.smis.campaignar.Fragments.FragmentYourInterestsDetail;
 import com.campaignar.smis.campaignar.R;
 
-public class KnowYourCandidate extends AppCompatActivity {
+public class KnowYourCandidate extends AppCompatActivity
+        implements FragmentKnowYourCandidate.OnFragmentInteractionListener,
+        FragmentKnowYourCandidateDetail.OnFragmentInteractionListener,
+        FragmentYourInterestsDetail.OnFragmentInteractionListener{
+
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_know_your_candidate);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            currentFragment = FragmentKnowYourCandidate.newInstance("","");
+            currentFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, currentFragment).commit();
+        }
+
+    }
+
+    @Override
+    public void onLoadFragmentKonwYourCandidateDetail() {
+
+        Fragment newFragment = FragmentKnowYourCandidateDetail.newInstance("","");
+        getSupportFragmentManager().beginTransaction()
+                .add(newFragment, "RING_SPECS")
+                .addToBackStack(null)
+                .replace(R.id.fragment_container, newFragment)
+                .commit();
+        currentFragment = newFragment;
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/campaignar/smis/campaignar/Adapter/KnowYourCandidateRecyclerViewAdapter.java
@@ -0,0 +1,78 @@
+package com.campaignar.smis.campaignar.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.campaignar.smis.campaignar.Fragments.FragmentKnowYourCandidate;
+import com.campaignar.smis.campaignar.R;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+/**
+ * Created by Vikas Kumar on 01-10-2016.
+ */
+
+public class KnowYourCandidateRecyclerViewAdapter extends
+        RecyclerView.Adapter<KnowYourCandidateRecyclerViewAdapter.KnowYourCandidateViewHolder> {
+
+    private final FragmentKnowYourCandidate.OnFragmentInteractionListener listener;
+    private Context mContext;
+
+    public KnowYourCandidateRecyclerViewAdapter(Context context, FragmentKnowYourCandidate.OnFragmentInteractionListener mListener) {
+        mContext = context;
+        listener = mListener;
+    }
+
+    @Override
+    public KnowYourCandidateViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View view = inflater.inflate(R.layout.custom_row_know_your_candidate, parent, false);
+
+        // Return a new holder instance
+        KnowYourCandidateViewHolder viewHolder = new KnowYourCandidateViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(KnowYourCandidateViewHolder holder, int position) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    public class KnowYourCandidateViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvState,tvPrevious,tvName,tvPosition;
+        CircularImageView candidateImageView;
+
+        public KnowYourCandidateViewHolder(View itemView) {
+            super(itemView);
+
+            tvState = (TextView) itemView.findViewById(R.id.tvState);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            tvPosition = (TextView) itemView.findViewById(R.id.tvPosition);
+            candidateImageView = (CircularImageView) itemView.findViewById(R.id.cvProfile);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onLoadFragmentKonwYourCandidateDetail();
+                }
+            });
+
+        }
+    }
+}
